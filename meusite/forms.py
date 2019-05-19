519e8da447d5366e0e5c2e1307636adaa4c6d025
AmandaSacchi <amandasacchi@hotmail.com>
2019-05-19 19:09:38 -0300

    Arrumei o cadastro e cadastrados

--- meusite/forms.py
+++ meusite/forms.py
@@ -3,17 +3,16 @@ from meusite.models import Candidato #nao cadastrar produto, apenas pedido
 
 class CandidatoForm(forms.ModelForm):
     class Meta:
-        model = Candidato 
-        #campos do formulario
-        fields = [
-        'nome',
-        'idade',
-        'genero',
-        'dt_nascimento',
-        'nacionalidade',
-        'jatrabalha',
-        'pretencao_salarial',
-        'perfil',
-        'foto'
+        model = Candidato    
+        fields = [    #campos do formulario
+            'nome',
+            'idade',
+            'genero',
+            'data_nascimento',
+            'nacionalidade',
+            'ja_trabalha',
+            'pretencao_salarial',
+            'perfil',
+            'foto'
         ]
 
--- meusite/models.py
+++ meusite/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Candidato(models.Model):
+    
     genero_feminino = 'f'
     genero_masculino = 'm'
     genero_outro = 'o'
@@ -13,14 +14,14 @@ class Candidato(models.Model):
         (genero_outro, 'Outro'),
     ]
 
-    nome = models.CharField(max_length=100)
+    nome = models.CharField(max_length=100, default=" ")
     idade = models.IntegerField()
-    genero = models.CharField(max_length=2, choices=genero_opcoes)
-    dt_nascimento = models.DateField()
-    nacionalidade = models.CharField(max_length=50)
-    jatrabalha = models.BooleanField(default=True)
+    genero = models.CharField(max_length=11, choices=genero_opcoes, default=" ")
+    data_nascimento = models.DateField()
+    nacionalidade = models.CharField(max_length=50, default=" ")
+    ja_trabalha = models.BooleanField(default=True)
     pretencao_salarial = models.DecimalField(decimal_places=2, max_digits=1000, default=0)
-    perfil = models.TextField()
+    perfil = models.TextField(default=" ")
     foto = models.ImageField(upload_to='', null=True)
 
     def __str__ (self):
